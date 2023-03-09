e355addad9d817472d7c8c521c68e9974149c106
BeatrizLandi <andressa.fideliscoelho@gmail.com>
2023-03-09 00:21:36 -0300

    half way

--- module/listaEstados.js
+++ module/listaEstados.js
@@ -3,20 +3,43 @@ const { estadosCidades } = require("./estados_cidades")
 const getListaDeEstados = function(){
 
     let siglas ={}
-
-    siglas.uf = estadosCidades.estados
-
- let siglasEstados = siglas
-
- return siglasEstados
     
-}
+ 
+    
+
+    let n = 0
+    
+     let numeroEstado = estadosCidades.estados.length
+     let cont = 0
+       
+     while(cont < numeroEstado){
+         
+         n +5
+     cont++
+       
+     }
+              
+    let listaUfs = [estadosCidades.estados[n].sigla]
+
+    siglas.uf = listaUfs
+
+    let siglasEstados = siglas
+
+    return siglasEstados
+        
+    }
+
+  
+    
+
 
 console.log(getListaDeEstados())
+
 
 console.log("quantidade :")
 console.log(estadosCidades.estados.length )
 
+//let tamanho = estadosCidades.length
 
 
 module.exports ={
