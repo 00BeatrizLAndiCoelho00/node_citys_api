2941de2edb2096109a3f732d1e39c44e9db6fcea
ANDRESSA BEATRIZ FIDELIS LANDI COELHO <22282076@CFP127ALUNO.LOCAL>
2023-03-09 10:04:20 -0300

    ugh

--- module/listaEstados.js
+++ module/listaEstados.js
@@ -2,41 +2,35 @@ const { estadosCidades } = require("./estados_cidades")
 
 const getListaDeEstados = function(){
 
-    let siglas ={}
+  
 
-    let n = 0 
+    estadosCidades.estados.forEach(estados => {
+          
+        let siglas ={}
+         
+        let listaUfs = [estados.sigla]
+        
+        //let lsitaLista = [listaUfs]
+
+        siglas.uf = listaUfs
    
-           
-    let listaUfs = [estadosCidades.estados[n].sigla]
-
-    siglas.uf = listaUfs
-
-    let siglasEstados = siglas
-
-    return siglasEstados
+        let a
+        a = estadosCidades.estados[1]
+        //console.log(typeof(listaUfs))
+        return a
+  
+        });
+        
+   
 }
 
-  
-    let numeroEstado = estadosCidades.estados.length
-    let cont = 0
-       
-    while(cont < numeroEstado){
-
-        let n = 0
-        n +  1
-        cont++
-        console.log(estadosCidades.estados[n].sigla)
-    }
-
-console.log(getListaDeEstados())
+ ////////////////////////////////////////////////////////////////////////////// 
 
 
-console.log("quantidade :")
-console.log(estadosCidades.estados.length )
-
-//let tamanho = estadosCidades.length
-
-
+ console.log(getListaDeEstados())
+ console.log("quantidade :")
+ console.log(estadosCidades.estados.length )
+ 
 module.exports ={
     getListaDeEstados
 }
